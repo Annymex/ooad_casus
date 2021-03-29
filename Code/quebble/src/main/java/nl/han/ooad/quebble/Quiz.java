202bd4faaf853f41aaf7c1910a5362ef72edde8a
Boris Otte <BAA.Otte@student.han.nl>
2021-03-29 15:36:05 +0200

    Quiz

--- Code/quebble/src/main/java/nl/han/ooad/quebble/Quiz.java
+++ Code/quebble/src/main/java/nl/han/ooad/quebble/Quiz.java
@@ -1,11 +1,25 @@
 package nl.han.ooad.quebble;
 
+import java.util.ArrayList;
+
 public class Quiz {
 
-	private Vraag[] vraag;
+	private ArrayList<Vraag> vragen;
+
+	public Quiz(ArrayList<Vraag> vragen) {
+		this.vragen = vragen;
+	}
+
+	public boolean heeftNogEenVraag (int index){
+		if (vragen.size() > index){
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	public Vraag getVraag(int index) {
-		return null;
+		return vragen.get(index);
 	}
 
 }
