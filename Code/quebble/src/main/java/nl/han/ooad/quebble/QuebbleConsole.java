236af7994a1017d28b3ae1af10bb5c396de36f6d
Boris Otte <BAA.Otte@student.han.nl>
2021-03-29 16:48:47 +0200

    verwijderen App

--- Code/quebble/src/main/java/nl/han/ooad/quebble/QuebbleConsole.java
+++ Code/quebble/src/main/java/nl/han/ooad/quebble/QuebbleConsole.java
@@ -1,5 +1,6 @@
 package nl.han.ooad.quebble;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class QuebbleConsole {
@@ -26,3 +27,20 @@ public class QuebbleConsole {
 
 
 }
+
+//	ArrayList<Antwoord> antwoordenVraag1 = new ArrayList<>();
+//        antwoordenVraag1.add(new MeerkeuzeAntwoord ("fout1", false));
+//				antwoordenVraag1.add(new MeerkeuzeAntwoord ("fout2", false));
+//				antwoordenVraag1.add(new MeerkeuzeAntwoord ("fout3", false));
+//				antwoordenVraag1.add(new MeerkeuzeAntwoord ("correct", true));
+//
+//				ArrayList<Antwoord> antwoordenVraag2 = new ArrayList<>();
+//		antwoordenVraag2.add(new KortAntwoordVraagAntwoord ("goed"));
+//		antwoordenVraag2.add(new KortAntwoordVraagAntwoord ("ook goed"));
+//
+//		ArrayList<Antwoord> antwoordenVraag3 = new ArrayList<>();
+//		antwoordenVraag3.add(new KortAntwoordVraagAntwoord ("goed"));
+//
+//		MeerkeuzeVraag vraag1 = new MeerkeuzeVraag(antwoordenVraag1);
+//		KortAntwoordVraag vraag2 = new KortAntwoordVraag(antwoordenVraag2);
+//		KortAntwoordVraag vraag3 = new KortAntwoordVraag(antwoordenVraag3);
