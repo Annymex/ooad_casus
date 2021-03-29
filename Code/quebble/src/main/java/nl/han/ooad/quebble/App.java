d76666d639fd6e7b8b6306867eed63770bdd1bec
Boris Otte <BAA.Otte@student.han.nl>
2021-03-29 14:45:34 +0200

    aanpassingen class diagram + maven project aanmaken

---
+++ Code/quebble/src/main/java/nl/han/ooad/quebble/App.java
@@ -0,0 +1,13 @@
+package nl.han.ooad.quebble;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
